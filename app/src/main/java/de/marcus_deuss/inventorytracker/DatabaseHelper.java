c8adc627652907f7034ca110255677204ef479df
Marcus Deuss <marcus.deuss@me.com>
2018-03-24 18:02:42 +0100

    added database layer, not tested yet

---
+++ app/src/main/java/de/marcus_deuss/inventorytracker/DatabaseHelper.java
@@ -0,0 +1,231 @@
+package de.marcus_deuss.inventorytracker;
+
+/**
+ * Created by marcus on 24.03.18.
+ */
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    // All Static variables
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+
+    // Database Name
+    private static final String DATABASE_NAME = "inventoryManager";
+
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    // Creating Table
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL(Inventory.CREATE_TABLE);
+    }
+
+    // Upgrading database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + Inventory.TABLE_NAME);
+
+        // Create tables again
+        onCreate(db);
+    }
+
+    /**
+     * All CRUD(Create, Read, Update, Delete) Operations
+     */
+
+    // Adding new inventory to database
+    // returns id of newly inserted row
+    public long InsertInventory(Inventory inventory) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Inventory.COLUMN_INVENTORYNAME, inventory.getInventoryName());
+        values.put(Inventory.COLUMN_DATEOFPURCHASE, inventory.getDateOfPurchase());
+        values.put(Inventory.COLUMN_PRICE, inventory.getPrice());
+        values.put(Inventory.COLUMN_INVOICE, inventory.getInvoice());
+        values.put(Inventory.COLUMN_TIMESTAMP, inventory.getTimeStamp());
+        values.put(Inventory.COLUMN_WARRANTY, inventory.getWarranty());
+        values.put(Inventory.COLUMN_SERIALNUMBER, inventory.getSerialNumber());
+        values.put(Inventory.COLUMN_IMAGE, inventory.getImage());
+        values.put(Inventory.COLUMN_REMARK, inventory.getRemark());
+        values.put(Inventory.COLUMN_OWNERNAME, inventory.getOwnerName());
+        values.put(Inventory.COLUMN_CATEGORYNAME, inventory.getCategoryName());
+        values.put(Inventory.COLUMN_BRANDNAME, inventory.getBrandName());
+        values.put(Inventory.COLUMN_ROOMNAME, inventory.getRoomName());
+
+        // Inserting Row
+        long id = db.insert(Inventory.TABLE_NAME, null, values);
+
+        db.close(); // Closing database connection
+
+        return id;
+    }
+
+    // Getting single inventory item
+    public Inventory getInventory(long id) {
+        // get readable database as we are not inserting anything
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(Inventory.TABLE_NAME,
+                new String[]{Inventory.COLUMN_ID, Inventory.COLUMN_INVENTORYNAME,
+                        Inventory.COLUMN_DATEOFPURCHASE, Inventory.COLUMN_PRICE, Inventory.COLUMN_INVOICE,
+                        Inventory.COLUMN_TIMESTAMP, Inventory.COLUMN_WARRANTY, Inventory.COLUMN_SERIALNUMBER,
+                        Inventory.COLUMN_IMAGE, Inventory.COLUMN_REMARK, Inventory.COLUMN_OWNERNAME,
+                        Inventory.COLUMN_CATEGORYNAME, Inventory.COLUMN_BRANDNAME, Inventory.COLUMN_ROOMNAME},
+                Inventory.COLUMN_ID + "=?",
+                new String[]{String.valueOf(id)}, null, null, null, null);
+
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        // prepare note object
+        Inventory inv = new Inventory(
+                cursor.getLong(cursor.getColumnIndex(Inventory.COLUMN_ID)),
+                cursor.getString(cursor.getColumnIndex(Inventory.COLUMN_INVENTORYNAME)),
+                cursor.getString(cursor.getColumnIndex(Inventory.COLUMN_DATEOFPURCHASE)),
+                cursor.getInt(cursor.getColumnIndex(Inventory.COLUMN_PRICE)),
+                cursor.getBlob(cursor.getColumnIndex(Inventory.COLUMN_INVOICE)),
+                cursor.getString(cursor.getColumnIndex(Inventory.COLUMN_TIMESTAMP)),
+                cursor.getInt(cursor.getColumnIndex(Inventory.COLUMN_WARRANTY)),
+                cursor.getString(cursor.getColumnIndex(Inventory.COLUMN_SERIALNUMBER)),
+                cursor.getBlob(cursor.getColumnIndex(Inventory.COLUMN_IMAGE)),
+                cursor.getString(cursor.getColumnIndex(Inventory.COLUMN_REMARK)),
+                cursor.getString(cursor.getColumnIndex(Inventory.COLUMN_OWNERNAME)),
+                cursor.getString(cursor.getColumnIndex(Inventory.COLUMN_CATEGORYNAME)),
+                cursor.getString(cursor.getColumnIndex(Inventory.COLUMN_BRANDNAME)),
+                cursor.getString(cursor.getColumnIndex(Inventory.COLUMN_ROOMNAME))
+        );
+
+        // close the db connection
+        cursor.close();
+
+        return inv;
+    }
+
+
+
+    // Getting all Inventory items
+    public List<Inventory> getAllInventory() {
+        List<Inventory> inventoryList = new ArrayList<Inventory>();
+
+        // Select All Query
+        String selectQuery = "SELECT  * FROM " + Inventory.TABLE_NAME + " ORDER BY " +
+                Inventory.COLUMN_INVENTORYNAME + " DESC";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                Inventory inv = new Inventory();
+                inv.setId(cursor.getLong(cursor.getColumnIndex(Inventory.COLUMN_ID)));
+                inv.setInventoryName(cursor.getString(cursor.getColumnIndex(Inventory.COLUMN_CATEGORYNAME)));
+                inv.setDateOfPurchase(cursor.getString(cursor.getColumnIndex(Inventory.COLUMN_DATEOFPURCHASE)));
+                inv.setPrice(cursor.getInt(cursor.getColumnIndex(Inventory.COLUMN_PRICE)));
+                inv.setInvoice(cursor.getBlob(cursor.getColumnIndex(Inventory.COLUMN_INVOICE)));
+                inv.setTimeStamp(cursor.getString(cursor.getColumnIndex(Inventory.COLUMN_TIMESTAMP)));
+                inv.setWarranty(cursor.getInt(cursor.getColumnIndex(Inventory.COLUMN_WARRANTY)));
+                inv.setSerialNumber(cursor.getString(cursor.getColumnIndex(Inventory.COLUMN_SERIALNUMBER)));
+                inv.setImage(cursor.getBlob(cursor.getColumnIndex(Inventory.COLUMN_IMAGE)));
+                inv.setRemark(cursor.getString(cursor.getColumnIndex(Inventory.COLUMN_REMARK)));
+                inv.setOwnerName(cursor.getString(cursor.getColumnIndex(Inventory.COLUMN_OWNERNAME)));
+                inv.setCategoryName(cursor.getString(cursor.getColumnIndex(Inventory.COLUMN_CATEGORYNAME)));
+                inv.setBrandName(cursor.getString(cursor.getColumnIndex(Inventory.COLUMN_BRANDNAME)));
+                inv.setRoomName(cursor.getString(cursor.getColumnIndex(Inventory.COLUMN_ROOMNAME)));
+
+                // Adding inventory to list
+                inventoryList.add(inv);
+            } while (cursor.moveToNext());
+        }
+
+        // close db connection
+        db.close();
+
+        // return inventory list
+        return inventoryList;
+    }
+
+
+
+    // Updating single inventory item
+    public long updateInventory(Inventory inv) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(Inventory.COLUMN_INVENTORYNAME, inv.getInventoryName());
+        values.put(Inventory.COLUMN_DATEOFPURCHASE, inv.getDateOfPurchase());
+        values.put(Inventory.COLUMN_PRICE, inv.getPrice());
+        values.put(Inventory.COLUMN_INVOICE, inv.getInvoice());
+        values.put(Inventory.COLUMN_WARRANTY, inv.getWarranty());
+        values.put(Inventory.COLUMN_SERIALNUMBER, inv.getSerialNumber());
+        values.put(Inventory.COLUMN_IMAGE, inv.getImage());
+        values.put(Inventory.COLUMN_REMARK, inv.getRemark());
+        values.put(Inventory.COLUMN_OWNERNAME, inv.getOwnerName());
+        values.put(Inventory.COLUMN_CATEGORYNAME, inv.getCategoryName());
+        values.put(Inventory.COLUMN_BRANDNAME, inv.getBrandName());
+        values.put(Inventory.COLUMN_ROOMNAME, inv.getRoomName());
+
+        // updating row
+        return db.update(Inventory.TABLE_NAME, values, Inventory.COLUMN_ID + " = ?",
+                new String[] { String.valueOf(inv.getId()) });
+    }
+
+
+    // Deleting single inventory item
+    public void deleteInventory(Inventory inv) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(Inventory.TABLE_NAME, Inventory.COLUMN_ID + " = ?",
+                new String[] { String.valueOf(inv.getId()) });
+        db.close();
+    }
+
+
+    // Getting inventory Count
+    public int getInventoryCount() {
+        String countQuery = "SELECT  * FROM " + Inventory.TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(countQuery, null);
+        cursor.close();
+
+        // return count
+        return cursor.getCount();
+    }
+
+
+    // insert some data into database as initial data
+    public void generateSampleData() {
+
+        Inventory inv = new Inventory((long) 0, "Esstisch", "12-10-2017", 399, null, "", 18, "nicht definiert", null, "keine Infos", "Marcus", "Möbel", "IKEA", "Wohnzimmer" );
+        this.InsertInventory(inv);
+
+        Inventory inv2 = new Inventory((long) 0, "Macbook Pro 13", "12-10-2016", 2399, null, "", 24, "nicht definiert", null, "keine Infos", "Marcus", "Computer", "Apple", "Wohnzimmer" );
+        this.InsertInventory(inv2);
+
+        Inventory inv3 = new Inventory((long) 0, "Thermomix", "12-09-2016", 1099, null,  "", 24, "nicht definiert", null, "keine Infos", "Sandra", "Küchengerät", "Thermomix", "Küche" );
+        this.InsertInventory(inv3);
+
+    }
+
+
+}
+
+
---
+++ app/src/main/java/de/marcus_deuss/inventorytracker/Inventory.java
@@ -0,0 +1,206 @@
+package de.marcus_deuss.inventorytracker;
+
+
+/**
+ * Created by marcus on 22.03.18.
+ */
+
+// contains inventory of an household
+public class Inventory {
+
+    // inventory table name
+    public static final String TABLE_NAME = "inventory";
+
+
+    // Inventory Table Column names
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_INVENTORYNAME = "inventoryname";
+    public static final String COLUMN_DATEOFPURCHASE = "dateofpurchase";
+    public static final String COLUMN_PRICE = "price";
+    public static final String COLUMN_INVOICE = "invoice";
+    public static final String COLUMN_TIMESTAMP = "timestamp";
+    public static final String COLUMN_WARRANTY = "warranty";
+    public static final String COLUMN_SERIALNUMBER = "serialnumber";
+    public static final String COLUMN_IMAGE = "image";
+    public static final String COLUMN_REMARK = "remark";
+    public static final String COLUMN_OWNERNAME = "ownername";
+    public static final String COLUMN_CATEGORYNAME = "categoryname";
+    public static final String COLUMN_BRANDNAME = "brandname";
+    public static final String COLUMN_ROOMNAME = "roomname";
+
+    // Create table SQL query string
+    public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
+            + COLUMN_ID + " INTEGER PRIMARY KEY," + COLUMN_INVENTORYNAME + " TEXT,"
+            + COLUMN_DATEOFPURCHASE  + " DATETIME," + COLUMN_PRICE + " INTEGER,"
+            + COLUMN_INVOICE + " BLOB," + COLUMN_TIMESTAMP + "DATETIME DEFAULT CURRENT_TIMESTAMP, "
+            + COLUMN_WARRANTY + " INTEGER," + COLUMN_SERIALNUMBER + " TEXT,"
+            + COLUMN_IMAGE + " BLOB," + COLUMN_REMARK + " TEXT,"
+            + COLUMN_OWNERNAME + " TEXT," + COLUMN_CATEGORYNAME + " TEXT,"
+            + COLUMN_BRANDNAME + " TEXT," + COLUMN_ROOMNAME + " TEXT" + ")";
+
+
+    // private variables
+    private long id;                       // database internal number, must be long!
+    private String inventoryName;        // name of inventory
+    private String dateOfPurchase;
+    private int price;
+    private byte[] invoice;               // PDF of already scanned invoice or new invoice rendered vom Jpeg to PDF
+    private String timeStamp;              // create date of database entry
+    private int warranty;                // warranty in number of months (6,12,18,24,36)
+    private String serialNumber;
+    private byte[] image;                // link to object image
+    private String remark;               // addl. info
+    private String ownerName;           // who owns this object in household
+    private String categoryName;        // (Technology, Computer, Furniture etc.)
+    private String brandName;       // (Apple, Sony, Samsung, IKEA etc.)
+    private String roomName;            // location of object
+
+    // Empty constructor
+    public Inventory(){
+
+    }
+
+    // constructor
+    public Inventory(long id, String inventoryName, String dateOfPurchase,
+                     int price, byte[] invoice, String timeStamp, int warranty,
+                     String serialNumber, byte[] image, String remark,
+                     String ownerName, String categoryName, String brandName,
+                     String roomName){
+        this.id = id;
+        this.inventoryName = inventoryName;
+        this.dateOfPurchase = dateOfPurchase;
+        this.price = price;
+        this.invoice = invoice;
+        this.timeStamp = timeStamp;
+        this.warranty = warranty;
+        this.serialNumber = serialNumber;
+        this.image = image;
+        this.remark = remark;
+        this.ownerName = ownerName;
+        this.categoryName = categoryName;
+        this.brandName = brandName;
+        this.roomName = roomName;
+    }
+
+
+    // getter methods
+    public long getId() {
+        return id;
+    }
+
+    public String getInventoryName() {
+        return inventoryName;
+    }
+
+    public String getDateOfPurchase() {
+        return dateOfPurchase;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public byte[] getInvoice() {
+        return invoice;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public int getWarranty() {
+        return warranty;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public byte[] getImage(){
+        return image;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    // setter methods
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setInventoryName(String inventoryName) {
+        this.inventoryName = inventoryName;
+    }
+
+    public void setDateOfPurchase(String dateOfPurchase) {
+        this.dateOfPurchase = dateOfPurchase;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setInvoice(byte[] invoice) {
+        this.invoice = invoice;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public void setWarranty(int warranty) {
+        this.warranty = warranty;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public void setImage(byte[] image){
+        this.image = image;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+}
+
+
+
+
+
+
