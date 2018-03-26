3ef7782bb102556a73e0cad41cb36d1faa614883
Marcus Deuss <marcus.deuss@me.com>
2018-03-26 11:16:45 +0200

    Edit inventory form added with sample data, not working yet - just for demo purpose

---
+++ app/src/main/java/de/marcus_deuss/inventorytracker/EditInventoryActivity.java
@@ -0,0 +1,76 @@
+package de.marcus_deuss.inventorytracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EditInventoryActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_inventory);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                finish();
+            }
+        });
+
+        // Spinner element
+        Spinner spinnerRoomName = (Spinner) findViewById(R.id.spinnerRoomname);
+
+        // Spinner click listener
+        spinnerRoomName.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("no Room");
+        categories.add("Kitchen");
+        categories.add("Living Room");
+        categories.add("Cellar");
+        categories.add("Bath Room");
+        categories.add("Office");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinnerRoomName.setAdapter(dataAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+    }
+
+}
--- app/src/main/java/de/marcus_deuss/inventorytracker/MainActivity.java
+++ app/src/main/java/de/marcus_deuss/inventorytracker/MainActivity.java
@@ -6,7 +6,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -46,14 +45,14 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+/*        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, R.string.toolTip, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
-
+*/
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -184,13 +183,18 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        Log.d(TAG, "menu selection");
+
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
             // TODO die einzelnen Menüeinträge implementieren und die id Namen ändern
-            sendEmail();
-            Toast.makeText(this, "Not implemented yet", Toast.LENGTH_SHORT).show();
+            //sendEmail();
+            //Toast.makeText(this, "Not implemented yet", Toast.LENGTH_SHORT).show();
+            Intent editForm = new Intent(this, EditInventoryActivity.class);
+            this.startActivity(editForm);
+
         } else if (id == R.id.nav_gallery) {
             Toast.makeText(this, "Not implemented yet", Toast.LENGTH_SHORT).show();
 
